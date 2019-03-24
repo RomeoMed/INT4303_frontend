be2d824dbbbcad5883e1ca8c1e5cc4a24c3e3e8d
RomeoMed <rmedoro@ltu.edu>
2019-03-24 14:41:33 -0400

    Added details and error templates

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import logging
 import json
+import requests
+import base64
 from logging.handlers import RotatingFileHandler
-from flask import Flask, request, abort, render_template
-#from flask_cors import CORS
+from flask import Flask, request, abort, render_template, session, jsonify
+# from flask_cors import CORS
 from functools import wraps
 from forms import *
-
 
 logPath = 'logs/frontend.log'
 _logger = logging.getLogger("progress_tracker")
@@ -29,6 +30,7 @@ def login_required(f):
         else:
             form = LoginForm(request.form)
             return render_template('forms/login.html', form=form)
+
     return wrap
 
 
@@ -39,23 +41,75 @@ def index():
 
 @app.route('/login')
 def login():
+    session['newUser'] = False
     form = LoginForm(request.form)
     return render_template('forms/login.html', form=form)
 
 
-@app.route('/main')
+@app.route('/register')
+def register():
+    session['newUser'] = True
+    form = RegisterForm(request.form)
+    return render_template('forms/register.html', form=form)
+
+
+@app.route('/main', methods=['POST'])
 def main():
+    new_user = session['newUser']
     email = request.form.get('name')
     psw = request.form.get('password')
-    #TODO: add api call to the progress_tracker_api
-    # if successful
-    # assign results to session variables for current session
-    # session['logged_id'] = True
-    # session['access_token'] = api_response['access_token']
-    # session['user_email'] = email
-    # and whatever else we may need to reference more than once.
+
+    session['user_email'] = email
+    data = {
+        'user_email': email,
+        'password': psw
+    }
+
+    if new_user:
+        url = 'signUpGate'
+    else:
+        url = 'signInGate'
+    endpoint = api_base_url + url
+    resp = requests.post(
+        endpoint, json=data,
+        headers={
+            'Content-Type': 'application/json',
+        }
+    )
+    result = json.loads(resp.text)
+
+    if result.get('code') >= 400:
+        return render_template('pages/error.html', result=result)
+
+    if result.get('code') <= 201:
+        session['access_token'] = result.get('access_token')
+
+        endpoint = api_base_url + 'getUserRole'
+        resp = requests.post(
+            endpoint, data=jsonify({'user_email': email}),
+            headers={
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + session['access_token']
+            }
+        )
+
+        result = json.loads(resp.txt)
+        if result.get('role') == 'Student':
+            if url == 'signUpGate':
+                form = DetailsForm(request.form)
+                return render_template('forms/details_form.html', form=form)
+            #TODO: get_user_details
+            # if details = first_login
+            # render details form
+            #else:
+                #render content html.
     return render_template('pages/content.html')
 
+
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+    form = DetailsForm(request.form)
+    return render_template('forms/details_form.html', form=form)
 
 @app.route('/forgot')
 def forgot():
@@ -64,7 +118,7 @@ def forgot():
 
 if __name__ == '__main__':
     # _logger.info('Server is Listening.....')
-    #TODO: temporary for testing. This will be removed from the py file
+    # TODO: temporary for testing. This will be removed from the py file
     # and placed in a secret key config file prior to launch.
     app.secret_key = 'f3cfe9ed8fae309f02079dbf'
-    app.run(debug=True)
+    app.run(debug=True, port=80)
--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
-from flask_wtf import Form
-from wtforms import PasswordField, StringField
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SelectField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 # Set your classes here.
 
 
-class RegisterForm(Form):
+class RegisterForm(FlaskForm):
     name = StringField(
         'Username', validators=[DataRequired(), Length(min=6, max=25)]
     )
@@ -22,12 +22,29 @@ class RegisterForm(Form):
     )
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     name = StringField('Username', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
 
 
-class ForgotForm(Form):
+class ForgotForm(FlaskForm):
     email = StringField(
         'Email', validators=[DataRequired(), Length(min=6, max=40)]
+    )
+
+
+class DetailsForm(FlaskForm):
+    first_name = StringField('First Name', [DataRequired()])
+    last_name = StringField('Last Name', [DataRequired()])
+    major = SelectField(
+        'Major',
+        [DataRequired()],
+        choices=[('BBA', 'Business Administration (BBA)'),
+                 ('BSIT', 'Information Technology (BSIT)')],
+    )
+    advisor = SelectField(
+        'Advisor',
+        [DataRequired()],
+        choices=[('sjanes@ltu.edu', 'Stefanie Janes'),
+                 ('test@test.edu', 'Test Advisor')]
     )
